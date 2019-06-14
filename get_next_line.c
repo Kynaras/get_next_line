9a77cea993f4b89dda10ca8876a47d543a22bfe0
Kyle EVERETT <keverett@c5r4s5.wethinkcode.co.za>
2019-06-14 11:51:44 +0200

    Made some theoretical progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 08:08:04 by keverett          #+#    #+#             */
-/*   Updated: 2019/06/12 16:02:48 by keverett         ###   ########.fr       */
+/*   Updated: 2019/06/14 11:51:27 by keverett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <stdlib.h>
 #include "libft.h"
 
+// Create a recursive function which keeps track of the number of buffs before assigning it to line.
 ft_readthis(const int fd, t_list *alst)
 {
 
@@ -26,22 +27,59 @@ ft_readthis(const int fd, t_list *alst)
 
 char	*get_next_line(const int fd, char **list)
 {
-	int i;
+	// Count counts the number of times recursion is used, buf1 to read to
+	static int count = 0;
 	char *buf1;
-	
+	int i;
+
+	i = 0;
+	count++;
 	// Assigns and allocates buffer to be read into
 	buf1 = ft_memalloc(BUFF_SIZE + 1);
 	read(fd, buf1, BUFF_SIZE);
 
 	//Reads the buffer to see if newline/EOF has been reached
-	while (buf1[i] != '\n' || buf1[i] != EOF)
+	while (buf1[i])
+	{
+		if (buf1[i] == '\n' || buf1[i] == EOF)
+			break;
 		i++;
+	}
+	//If no new line, recursive call to get_next_line. Else if newline/EOF is found, malloc/write;
 	if (i = BUFF_SIZE)
-
-
-
-
-
-	return (ytr);
+		get_next_line(fd, list);
+	else
+	{
+		*list = (char*)ft_memalloc(count * BUFF_SIZE + i + 1);
+		i--;
+		while (i >= 0)
+		{
+			*list[count * BUFF_SIZE + i] = buf1[i];
+			i++;
+		}
+	}
+	// Second call for full BUFF_SIZE for the full buffers in the recursion
+	if (i = BUFF_SIZE)
+	{
+		i--;
+		count--;
+		while (i >= 0);
+		{
+			*list[count * BUFF_SIZE + i] = buf1[i];
+			i--;
+		}
+	}
+	return (0);
 }
+
+int main()
+{
+	char **c;
+	char b;
+	int i;
+	c = &b;
+	b = NULL;
+	
+	
+	get_next_line
 
