37f5cb049117e0c44bc4291b125c8b4febef6082
Kyle EVERETT <keverett@c5r4s5.wethinkcode.co.za>
2019-06-19 09:40:48 +0200

    Safety first

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,40 @@
 /*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 08:27:28 by keverett          #+#    #+#             */
-/*   Updated: 2019/06/18 16:16:52 by keverett         ###   ########.fr       */
+/*   Updated: 2019/06/19 09:40:41 by keverett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 void	st_counter(b_list *link)
 
+void ft_count(char **line, t_list **alst)
+{
+	t_list *ptr;
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	ptr = *alst;
+	while (ptr != NULL)
+	{
+		while (ptr.content[i] != '\0')
+		{
+			if (ptr.content[i] == '\n' || ptr.content == EOF)
+				break;
+			i++;
+			count++;
+		}
+		i = 0;
+	}
+	*line = (char*)malloc(sizeof(char) * count + 1);
+		
+		
 int	get_next_line(int fd, char **line)
 {
 	static t_list **alst;
+	t_list *ptr;
 	char *buf;
 	int i;
 	int nextline;
@@ -23,7 +48,7 @@ int	get_next_line(int fd, char **line)
 	nextline = 1;
 	i = 0;
 	buf = ft_memalloc(BUFF_SIZE + 1);
-	alst = malloc(sizeof(t_list);
+	alst = malloc(sizeof(*t_list));
 
 	read(buf, BUFF_SIZE);
 	while(buf[i])
@@ -38,8 +63,16 @@ int	get_next_line(int fd, char **line)
 	else
 		*alst = ft_lstnew(buf, 0);
 	if (i == BUFF_SIZE)
-		ft_lstadd
+		ft_lstadd(t_list **alst, ft_lstnew(buf, 0));
+
+
+}
+int main()
+{
+	int id;
+	id = open("test", O_RDONLY);
+	get_next_line(id, 
 	
-	
+
 
 
