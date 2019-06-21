fb2a971cb4f4120f48a808548702def285fe6466
Kyle EVERETT <keverett@c5r4s5.wethinkcode.co.za>
2019-06-21 17:04:16 +0200

    Thanks Alistair

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 /*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 10:34:55 by keverett          #+#    #+#             */
-/*   Updated: 2019/06/21 08:42:13 by keverett         ###   ########.fr       */
+/*   Updated: 2019/06/21 16:07:22 by keverett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFF_SIZE
-# define BUFF_SIZE 5
+# define BUFF_SIZE 1
 
 #endif
