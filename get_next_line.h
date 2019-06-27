0eacf0264a586f6791bfc8504469f1dcb8927872
Kyle EVERETT <keverett@c5r5s3.wethinkcode.co.za>
2019-06-27 16:00:15 +0200

    Working?

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 /*   By: keverett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 10:34:55 by keverett          #+#    #+#             */
-/*   Updated: 2019/06/21 16:07:22 by keverett         ###   ########.fr       */
+/*   Updated: 2019/06/27 15:59:29 by keverett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFF_SIZE
-# define BUFF_SIZE 500
+# define BUFF_SIZE 37
 
 #endif
--- reduu.c
+++ reduu.c
@@ -1,8 +1,11 @@
 #include "get_next_line.h"
 #include "libft.h"
 #include <sys/types.h>
-       #include <sys/stat.h>
-       #include <fcntl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
 
 int	ft_checkline(char *buf)
 {
@@ -13,7 +16,7 @@ int	ft_checkline(char *buf)
 	{
 		if (buf[0] == '\n')
 			return (-1);
-		else if (buf[i] == '\n')
+		if (buf[i] == '\n')
 			return (i);
 		i++;
 	}
@@ -25,45 +28,76 @@ int	ft_checkline(char *buf)
 
 int	get_next_line(int fd, char **line)
 {
-	static char* buf;
-	char *tmp;
+	static t_list *list;
+	t_list *tmp;
+	char *temp;
+	char* buf;
 	int loop;
 	int rd;
 	int check;
 
+	list = NULL;
 	*line = ft_memalloc(1);
 	loop = 0;
 	buf = ft_memalloc(BUFF_SIZE + 1);
 
-	while (loop == 0);
+	while (loop == 0)
 	{
+		ft_bzero(buf, BUFF_SIZE);
 		rd = read(fd, buf, BUFF_SIZE);
 		check = ft_checkline(buf);
-		if (check = -1)
-		{
+		/*	if (check == -1)
+			{
 			loop = 1;
 			return(1);
-		}
-		else if (check = -2)
+			}*/
+		if (check == -2)
 		{
-			tmp = ft_strjoin(*line, buf);
+			temp = ft_strjoin(*line, buf);
 			free(*line);
-			*line = ft_strdup(tmp);
-			free(tmp);
-			loop = 1;
+			*line = ft_strdup(temp);
+			free(temp);
 		}
 		else
 		{
-			tmp = ft_strjoin(*line, ft_strsub(buf, 0, check));
-			free(*line);
-			*line = ft_strdup(tmp);
-			free(tmp);
+			if (check > 0)
+			{
+				temp = ft_strjoin(*line, ft_strsub(buf, 0, check));
+				free(*line);
+				*line = ft_strdup(temp);
+				free(temp);
+			}
 			loop = 1;
+			if (list == NULL)
+			{
+				printf("%s", (ft_strchr(buf, '\n') + 1));
+				list = ft_lstnew((ft_strchr(buf, '\n') + 1), BUFF_SIZE + 1);
+				list->content_size = fd;
+			}
+			else
+			{
+				tmp = list;
+				while (tmp != NULL)
+				{
+					if (tmp->content_size == fd)
+					{
+						ft_bzero(tmp->content, BUFF_SIZE);
+						ft_strcpy(tmp->content, ft_strchr(buf, '\n') + 1);
+						break;
+					}
+					else
+						tmp = tmp->next;
+				}
+				if (tmp == NULL)
+				{
+					ft_lstadd(&list, ft_lstnew((ft_strchr(buf, '\n') + 1), BUFF_SIZE + 1));
+					list->content_size = fd;
+				}
+			}
 		}
-		loop = 1;
 	}
-
-return (0);	
+	printf("%s\n", list->content);
+	return (0);
 }
 int main()
 {
